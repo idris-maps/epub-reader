c84ea260623c1807a9f9da90bf5c4c9f79586644
Anders <idris.maps@gmx.ch>
2021-01-10 22:35:18 +0100

    Fix typos

--- src/components/Info.svelte
+++ src/components/Info.svelte
@@ -1,8 +1,8 @@
 <div class="info">
   <h2>What?</h2>
-  <p><a href="https://en.wikipedia.org/wiki/EPUB">.epub</a> is the most common e-book format. Here you can read your books in the browser.</p>
-  <p>The books will be saved in browser memory. You can bookmark the page to come back later. Clearing the browser memory will delete them. No files are sent to remote servers.</p>
-  <p>We do not do not collect any usage data. The source code and available <a href="https://github.com/idris-maps/epub-reader">here</a>.</p>
+  <p><a href="https://en.wikipedia.org/wiki/EPUB">.epub</a> is the most common e-book format. Here you can read your books online.</p>
+  <p>The books will be saved in browser memory. You can bookmark the page to come back later. Clearing the browser cache will delete them. No files are sent to remote servers.</p>
+  <p>We do not do not collect any usage data. The source code is available <a href="https://github.com/idris-maps/epub-reader">here</a>.</p>
   <p>A selection of sites where you can find books in the .epub format:</p>
   <ul>
     <li>
