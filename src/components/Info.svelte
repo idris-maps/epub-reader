f3c9553a416e45bc262ec2d24659ee47f7c461ff
Anders <12154856+idris-maps@users.noreply.github.com>
2023-01-04 23:05:57 +0100

    Fixes/2023 01 04 (#2)
    * Fix info
    
    * Add keyboard commands on drop zone
    
    * Random fixes

--- src/components/Info.svelte
+++ src/components/Info.svelte
@@ -1,39 +1,41 @@
 <div class="info">
-  <h2>What?</h2>
-  <p><a href="https://en.wikipedia.org/wiki/EPUB">.epub</a> is the most common e-book format. Here you can read your books online.</p>
-  <p>The books will be saved in browser memory. You can bookmark the page to come back later. Clearing the browser cache will delete them. No files are sent to remote servers.</p>
-  <p>We do not do not collect any usage data. The source code is available <a href="https://github.com/idris-maps/epub-reader">here</a>.</p>
+  <h2>What is this?</h2>
+  <p><a href="https://en.wikipedia.org/wiki/EPUB" title="epub">.epub</a> is an e-book format. This app lets you read them in the browser.</p>
+  <p>The books will be saved in browser memory. You can bookmark the page to come back later. Clearing the browser cache will delete them.</p>
+  <p>No files are sent to remote servers. No usage data collected. The source code is available <a href="https://github.com/idris-maps/epub-reader" title="epub-reader source">here</a>.</p>
+  <h3>Where to find books?</h3>
   <p>A selection of sites where you can find books in the .epub format:</p>
   <ul>
     <li>
-      <a href="https://standardebooks.org/">
+      <a href="https://standardebooks.org/" title="Standard ebooks">
         standardebooks.org 
       </a>
       - well crafted public domain books
     </li>
     <li>
-      <a href="https://gutenberg.org/">
+      <a href="https://gutenberg.org/" title="Gutenberg">
         gutenberg.org 
       </a>
       - big selection of public domain books
     </li>
     <li>
-      <a href="https://duckduckgo.com/?q=libgen">
+      <a href="https://duckduckgo.com/?q=libgen" title="Libgen">
         libgen 
       </a>
       - file sharing based library, dubious legality
     </li>
     <li>
-      <a href="https://www.ebooksgratuits.com/">
+      <a href="https://www.ebooksgratuits.com/" title="e-books gratuits">
         ebooksgratuits.com
       </a>
       - public domain books in French
     </li>
     <li>
-      <a href="https://ebooks-bnr.com/">
+      <a href="https://ebooks-bnr.com/" title="Bibliothèque numérique romande">
         ebooks-bnr.com
       </a>
       - public domain books in French with focus on Switzerland
     </li>
   </ul>
+  <p>To create your own books from markdown files, you can use <a href="https://github.com/idris-maps/md2epub#md2epub" title="md2epub">this tool</a>.</p>
 </div>
